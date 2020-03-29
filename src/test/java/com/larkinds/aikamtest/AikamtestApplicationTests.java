ce3efa3446b5ad7380d47514c6218236140281ee
larkinds <larkindsx@gmail.com>
2020-03-29 19:52:46 +0700

    Initial commit

---
+++ src/test/java/com/larkinds/aikamtest/AikamtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larkinds.aikamtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AikamtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
