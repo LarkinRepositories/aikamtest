84692d20f38aac81dfc87da79c162328cee0f443
larkinds <larkindsx@gmail.com>
2020-03-30 21:47:02 +0700

    оптимизировал импорты

--- src/main/java/com/larkinds/aikamtest/deserializer/SearchDeserializer.java
+++ src/main/java/com/larkinds/aikamtest/deserializer/SearchDeserializer.java
@@ -10,8 +10,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.larkinds.aikamtest.dto.fromjson.criteria.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.ArrayList;
