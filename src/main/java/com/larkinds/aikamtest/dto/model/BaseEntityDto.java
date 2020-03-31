d321b23b86ba0ca99ef596746ed34f514a26a8a6
larkinds <larkindsx@gmail.com>
2020-03-31 17:48:53 +0700

    разметил для корректной сериализации

--- src/main/java/com/larkinds/aikamtest/dto/model/BaseEntityDto.java
+++ src/main/java/com/larkinds/aikamtest/dto/model/BaseEntityDto.java
@@ -1,8 +1,10 @@
 package com.larkinds.aikamtest.dto.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class BaseEntityDto {
+    @JsonIgnore
     private Long id;
 }
