1f6e19d9b5f65e1218adb6ba13a220f8664fe7b2
larkinds <larkindsx@gmail.com>
2020-03-31 18:28:32 +0700

    убрал @Sneakythrows, добавил необходимые проверки

--- src/main/java/com/larkinds/aikamtest/service/impl/MainServiceImpl.java
+++ src/main/java/com/larkinds/aikamtest/service/impl/MainServiceImpl.java
@@ -4,22 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.larkinds.aikamtest.dto.fromjson.input.SearchDto;
 import com.larkinds.aikamtest.dto.fromjson.input.StatDto;
 import com.larkinds.aikamtest.dto.tojson.output.CriteriaOutputDto;
+import com.larkinds.aikamtest.dto.tojson.output.MyError;
 import com.larkinds.aikamtest.dto.tojson.output.OutputDto;
 import com.larkinds.aikamtest.dto.tojson.output.SearchOutPut;
-import com.larkinds.aikamtest.dto.tojson.output.StatOutPut;
 import com.larkinds.aikamtest.service.MainService;
 import com.larkinds.aikamtest.service.SearchService;
 import com.larkinds.aikamtest.service.StatService;
-import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class MainServiceImpl implements MainService {
     private ObjectMapper mapper;
     private SearchService searchService;
@@ -35,25 +37,52 @@ public class MainServiceImpl implements MainService {
         this.statService = statService;
     }
 
-    @SneakyThrows
     @Override
-    public OutputDto processAndWrite(String... args) {
-        String operation = args[0].toLowerCase();
-        if (SEARCH_OPERATION.equals(operation)) {
-            SearchDto searchDto = mapper.readValue(new File(args[1]), SearchDto.class);
-            List<CriteriaOutputDto> results = new ArrayList<>();
-            searchDto.getCriteria().forEach(e->{
-                results.add(new CriteriaOutputDto(e, e.getResult(searchService)));
-            });
-            SearchOutPut searchOutPut = new SearchOutPut(results);
-            mapper.writeValue(new File(args[2]), searchOutPut);
-            return searchOutPut;
-        } else if (STAT_OPERATION.equals(operation)) {
-            StatDto statDto = mapper.readValue(new File(args[1]), StatDto.class);
-            StatOutPut stat = statService.getStat(LocalDate.parse(statDto.getStartDate()), LocalDate.parse(statDto.getEndDate()));
-            mapper.writeValue(new File(args[2]), stat);
-            return stat;
+    public OutputDto processAndWrite(String... args) throws Exception {
+        final String operation = args[0].toLowerCase();
+        final String inputFile = args[1];
+        final String outputFile = args[2];
+        OutputDto dto = null;
+        try {
+            if (SEARCH_OPERATION.equals(operation)) {
+                SearchDto searchDto = null;
+                try {
+                    searchDto = mapper.readValue(new File(inputFile), SearchDto.class);
+                } catch (IOException e) {
+                    log.warn(e.getMessage());
+                }
+                List<CriteriaOutputDto> results = new ArrayList<>();
+                assert searchDto != null;
+                searchDto.getCriteria().forEach(e -> {
+                    results.add(new CriteriaOutputDto(e, e.getResult(searchService)));
+                });
+                dto = new SearchOutPut(results);
+                try {
+                    mapper.writeValue(new File(outputFile), dto);
+                } catch (IOException e) {
+                    log.warn(e.getMessage());
+                }
+            } else if (STAT_OPERATION.equals(operation)) {
+                StatDto statDto = null;
+                try {
+                    statDto = mapper.readValue(new File(inputFile), StatDto.class);
+                } catch (IOException e) {
+                    log.warn(e.getMessage());
+                }
+                assert statDto != null;
+                dto = statService.getStat(LocalDate.parse(statDto.getStartDate()), LocalDate.parse(statDto.getEndDate()));
+                try {
+                    mapper.writeValue(new File(outputFile), dto);
+                } catch (IOException e) {
+                    log.warn(e.getMessage());
+                }
+            } else {
+                throw new Exception("Unsupported operation");
+            }
+        } catch (Exception e) {
+            MyError error = new MyError(e.getMessage());
+            mapper.writeValue(new File(outputFile), error);
         }
-        return null;
+        return dto;
     }
 }
