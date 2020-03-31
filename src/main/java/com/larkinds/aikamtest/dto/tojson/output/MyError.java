1c1bd846d7310f58963e204662fc8bb389c1d403
larkinds <larkindsx@gmail.com>
2020-03-31 18:33:22 +0700

    разметил для корректного порядка полей

--- src/main/java/com/larkinds/aikamtest/dto/tojson/output/MyError.java
+++ src/main/java/com/larkinds/aikamtest/dto/tojson/output/MyError.java
@@ -2,9 +2,11 @@ package com.larkinds.aikamtest.dto.tojson.output;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Value;
 
 @Value
+@JsonPropertyOrder({"type", "message"})
 public class MyError extends OutputDto {
     @JsonProperty("message")
     String message;
